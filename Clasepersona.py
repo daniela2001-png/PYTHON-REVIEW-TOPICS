fab2d175a21ea258a7a03283b3b7b3e624f35d54
daniela2001-png <dmbmora@outlook.com>
2021-04-19 21:37:13 -0500

    mañan continuare con esto :3

---
+++ Clasepersona.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+class Persona():
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def saludar(self):
+        return f"hola {self.nombre}"
+
+
+daniela = Persona("daniela")
+daniela.saludar()
